7ebc21e4972b5819a73d24764668819361e6bf31
Yrocky <983272765@qq.com>
2015-11-23 14:12:45 +0800

    Initial Commit

---
+++ HLLMediaPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLMediaPlay
+//
+//  Created by admin on 15/11/23.
+//  Copyright © 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLMediaPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLMediaPlay
+//
+//  Created by admin on 15/11/23.
+//  Copyright © 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
